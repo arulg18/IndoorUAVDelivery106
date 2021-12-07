edde15324d153f3f26fc93d9f8e02a1e6b00c5d6
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2021-12-06 23:54:57 -0800

    Added updated code

--- Reading_Sensors/DW1000Ranging_ANCHOR_3/DW1000Ranging_ANCHOR_3.ino
+++ Reading_Sensors/DW1000Ranging_ANCHOR_3/DW1000Ranging_ANCHOR_3.ino
@@ -4,18 +4,31 @@
  *  - move strings to flash (less RAM consumption)
  *  - fix deprecated convertation form string to char* startAsAnchor
  *  - give example description
+ *  
  */
+
+#define USE_USBCON
+#include <ros.h>
 #include <SPI.h>
 #include "DW1000Ranging.h"
+#include <ros/time.h>
+#include <localization/TimestampDistance.h>
 
 // connection pins
 const uint8_t PIN_RST = 4; // reset pin
 const uint8_t PIN_IRQ = 7; // irq pin
 const uint8_t PIN_SS = 3; // spi select pin
 
+
+//ros variables
+ros::NodeHandle nh;
+localization::TimestampDistance distance_msg;
+ros::Publisher range_pub("distance_3", &distance_msg);
+
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
+  nh.getHardware()->setBaud(115200);
+  nh.initNode();
+  nh.advertise(range_pub);
   //init the configuration
   DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
   //define the sketch as anchor. It will be great to dynamically change the type of module
@@ -24,29 +37,44 @@ void setup() {
   DW1000Ranging.attachInactiveDevice(inactiveDevice);
   byte id[2] = {1, 1};
   //Enable the filter to smooth the distance
-  DW1000Ranging.useRangeFilter(true);
+//  DW1000Ranging.useRangeFilter(true);
   //we start the module as an anchor
-  DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", id, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
+  DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", id, DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+
 }
 
 void loop() {
   DW1000Ranging.loop();
+//  Serial.print(distance_msg.header.stamp);
+//  Serial.print('\t');
+//  Serial.println(distance_msg.distance);
 //  newRange();
 }
 
 void newRange() {
-  Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-  Serial.print("\t Range: "); Serial.print(DW1000Ranging.getDistantDevice()->getRange()); Serial.print(" m");
-  Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
+  distance_msg.distance = DW1000Ranging.getDistantDevice()->getRange();
+  distance_msg.header.stamp = nh.now();
+  range_pub.publish(&distance_msg);
+  nh.spinOnce();
+  delay(3);
+//  int timE = (DW1000Ranging.getDistantDevice()->timeRangeReceived.getAsMicroSeconds())/(1000);
+//  Serial.print("\t from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+//  Serial.print("\t Range: "); Serial.print(range); Serial.print(" m");
+//  Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
+//  Serial.print("\t Time Stamp: "); Serial.print(time); Serial.println(" ms");
+
+
+
+//  int time = DW1000Ranging.getDistantDevice()->timeRangeReceived.getAsMicroSeconds();
 }
 
 void newBlink(DW1000Device* device) {
-  Serial.print("blink; 1 device added ! -> ");
-  Serial.print(" short:");
-  Serial.println(device->getShortAddress(), HEX);
+//  Serial.print("blink; 1 device added ! -> ");
+//  Serial.print(" short:");
+//  Serial.println(device->getShortAddress(), HEX);
 }
 
 void inactiveDevice(DW1000Device* device) {
-  Serial.print("delete inactive device: ");
-  Serial.println(device->getShortAddress(), HEX);
+//  Serial.print("delete inactive device: ");
+//  Serial.println(device->getShortAddress(), HEX);
 }
